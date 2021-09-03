03fbd65f597d9fd0ceb25d0e0deb33822156d490
Fabian Jocks <fabian.jocks@hcom.de>
2021-09-03 15:15:59 +0200

    Increased php version to php 8 and fixed test execution directory mount

--- bin/runTests.sh
+++ bin/runTests.sh
@@ -3,5 +3,5 @@ SCRIPT_DIR=$(dirname "$(readlink -e "$0")")
 
 docker run -it --rm --name doctrine1 \
   -v "${SCRIPT_DIR}/../:/usr/src/doctrine1" \
-  -w "/usr/src/doctrine1" \
-    php:7.2 php tests/index.php ${@}
+  -w "/usr/src/doctrine1/var/tmp" \
+    php:8 php ../../tests/index.php ${@}
